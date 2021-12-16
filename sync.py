38ab6470aee41b13b74136adb6307a8e0c14324c
Dmitry <onishchuk.dmitry@gmail.com>
2021-12-16 17:40:57 +0300

    sync.py complete

---
+++ sync.py
@@ -0,0 +1,166 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta
+import hashlib
+import os
+import shutil
+import stat
+import time
+import logging
+import sys
+import signal
+
+RUN = True
+
+
+def signal_handler(signal, frame):
+    """ soft interrupt with 'ctrl+c' """
+    global RUN
+    logging.info("Stopped by user. Synchronization ending...please wait.")
+    RUN = False
+
+
+def make_meta(src, meta=None):
+    """creating metadata dictionary"""
+    if meta is None:
+        meta = {}
+    for name in os.listdir(src):
+        path = os.path.join(src, name)
+        if not os.path.isfile(path):
+            make_meta(path, meta)
+        else:
+            with open(path, mode='rb') as f:
+                meta[path] = hashlib.md5(f.read()).hexdigest()
+    return meta
+
+
+def make_tree(src):
+    """creating directory tree"""
+    tree = [(root, dirs, files) for root, dirs, files in os.walk(src)]
+    return tree
+
+
+def del_file_and_dir(deleted):
+    if os.path.isdir(deleted):
+        tree = make_tree(deleted)
+        for elem in tree[::-1]:
+            root, dirs, files = elem
+            for file in files:
+                logging.info(msg=f'Delete file "{os.path.basename(file)}" from "{root}"')
+            logging.info(msg=f'Delete directory "{root}"')
+        shutil.rmtree(deleted)
+    else:
+        os.chmod(deleted, stat.S_IWRITE)
+        os.remove(deleted)
+        logging.info(msg=f'Delete file "{os.path.basename(deleted)}" from "{os.path.dirname(deleted)}"')
+    assert not os.path.exists(deleted)
+
+
+def copy(src, dst):
+    if os.path.exists(src):
+        shutil.copy(src, dst)
+        logging.info(msg=f'File "{os.path.basename(src)}" coped from '
+                         f'"{os.path.dirname(src)}" to "{os.path.dirname(dst)}"')
+
+
+def check(src, dst):
+    """checking the identity of directories"""
+    logging.info(msg='Checking...')
+    tree_src = make_tree(src=src)
+    tree_dst = make_tree(src=src)
+    meta_src = make_meta(src=src)
+    meta_dst = make_meta(src=dst)
+
+    if len(meta_src.keys()) == len(meta_dst.keys()):
+        for file_s_hash, file_d_hash in zip(meta_src.values(), meta_dst.values()):
+            if file_s_hash != file_d_hash:
+                return False
+    else:
+        return False
+
+    if len(tree_src) == len(tree_dst):
+        for source, target in zip(tree_src, tree_dst):
+            if len(source[1]) == len(target[1]):
+                for dir_s, dir_t in zip(source[1], target[1]):
+                    if dir_s != dir_t:
+                        return False
+            else:
+                return False
+    else:
+        return False
+
+    return True
+
+
+def sync_dirs(src: str, dst: str, meta: dict, tree: list, new_meta=None, new_tree=None):
+    """directory synchronization"""
+    logging.info(f'Update information...')
+    if new_meta is None:
+        new_meta = make_meta(src)
+
+    if new_tree is None:
+        new_tree = make_tree(src)
+
+    for element in tree:
+        root, files = element[::2]
+        new_root = root.replace(src, dst)
+        if not os.path.exists(new_root) and os.path.exists(root):
+            os.mkdir(new_root)
+            logging.info(msg=f'Create directory "{new_root}"')
+
+        for file in files:
+            new_path_to_file = os.path.join(new_root, file)
+            old_path_to_file = os.path.join(root, file)
+
+            if old_path_to_file in new_meta and not os.path.exists(new_path_to_file):
+                copy(src=old_path_to_file, dst=new_path_to_file)
+            elif os.path.exists(new_path_to_file) and not os.path.exists(old_path_to_file):
+                del_file_and_dir(new_path_to_file)
+            elif old_path_to_file in new_meta and meta[old_path_to_file] != new_meta[old_path_to_file]:
+                copy(src=old_path_to_file, dst=new_path_to_file)
+
+        if os.path.exists(new_root) and not os.path.exists(root):
+            del_file_and_dir(new_root)
+
+    return new_meta, new_tree
+
+
+def main():
+    try:
+        source, destination, interval, logs = (int(elem) if elem.isdigit() else elem for elem in sys.argv[1:])
+    except ValueError:
+        print("Invalid number of arguments!")
+        return
+
+    source = os.path.abspath(source)
+    destination = os.path.abspath(destination)
+    logs = os.path.abspath(logs)
+    path_log = os.path.join(logs, 'logs.txt')
+    os.makedirs(logs, exist_ok=True)
+
+    logging.basicConfig(format='%(asctime)s | %(message)s',
+                        datefmt='%m.%d.%Y %H:%M:%S',
+                        level=logging.INFO,
+                        handlers=[
+                            logging.StreamHandler(stream=sys.stdout),
+                            logging.FileHandler(path_log, mode='a')
+                        ],)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    logging.info(f'Start sync "{os.path.abspath(source)}" and "{os.path.abspath(destination)}"')
+
+    meta = make_meta(source)
+    tree = make_tree(source)
+    while RUN:
+        meta, tree = sync_dirs(src=source, dst=destination, meta=meta, tree=tree)
+        if not check(src=source, dst=destination):
+            logging.info('Resynchronization...')
+            meta, tree = sync_dirs(src=source, dst=destination, meta=meta, tree=tree)
+        logging.info(f'Synchronization complete. '
+                     f'Next in {(datetime.now() + timedelta(seconds=interval)).time().strftime("%H:%M:%S")}')
+        time.sleep(interval)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_sync.py
@@ -0,0 +1,107 @@
+import os
+from sync import sync_dirs, make_meta, make_tree, check
+import shutil
+
+DEL_AFTER_TEST = True
+
+
+def make_test_folder(name):
+    cur_dir = os.getcwd()
+    test_source_path = os.path.join(cur_dir, name)
+    try:
+        os.mkdir(test_source_path)
+    except FileExistsError:
+        shutil.rmtree(test_source_path)
+        os.mkdir(test_source_path)
+    return test_source_path
+
+
+def make_test_file(path, name):
+    path_to_file = os.path.join(path, name)
+    file = open(path_to_file, mode='w')
+    file.close()
+    return path_to_file
+
+
+def test_empty_folder():
+    source = make_test_folder(name='test_source')
+    target = make_test_folder(name='test_target')
+    sync_dirs(
+        src=source,
+        dst=target,
+        meta=make_meta(src=source),
+        tree=make_tree(src=source)
+    )
+    assert check(source, target), 'Folders are not identical!'
+    if DEL_AFTER_TEST:
+        shutil.rmtree(source)
+        shutil.rmtree(target)
+
+
+def test_with_file():
+    source = make_test_folder(name='test_source')
+    target = make_test_folder(name='test_target')
+    make_test_file(path=source, name='test.txt')
+    sync_dirs(
+        src=source,
+        dst=target,
+        meta=make_meta(src=source),
+        tree=make_tree(src=source)
+    )
+    assert check(source, target), 'Folders are not identical!'
+    if DEL_AFTER_TEST:
+        shutil.rmtree(source)
+        shutil.rmtree(target)
+
+
+def test_file_edit():
+    source = make_test_folder(name='test_source')
+    target = make_test_folder(name='test_target')
+    path_to_file = make_test_file(path=source, name='test.txt')
+    meta, tree = sync_dirs(
+        src=source,
+        dst=target,
+        meta=make_meta(src=source),
+        tree=make_tree(src=source)
+    )
+    assert check(source, target), 'Folders are not identical!'
+
+    file_ = open(path_to_file, mode='a')
+    file_.write("I'm new in this file!")
+    file_.close()
+    sync_dirs(
+        src=source,
+        dst=target,
+        meta=meta,
+        tree=tree
+    )
+    assert check(source, target), 'Folders are not identical after edit!'
+
+    if DEL_AFTER_TEST:
+        shutil.rmtree(source)
+        shutil.rmtree(target)
+
+
+def test_file_remove():
+    source = make_test_folder(name='test_source')
+    target = make_test_folder(name='test_target')
+    path_to_file = make_test_file(path=source, name='test.txt')
+    meta, tree = sync_dirs(
+        src=source,
+        dst=target,
+        meta=make_meta(src=source),
+        tree=make_tree(src=source)
+    )
+    assert check(source, target), 'Folders are not identical!'
+
+    os.remove(path_to_file)
+    sync_dirs(
+        src=source,
+        dst=target,
+        meta=meta,
+        tree=tree
+    )
+    assert check(source, target), 'Folders are not identical after remove!'
+    if DEL_AFTER_TEST:
+        shutil.rmtree(source)
+        shutil.rmtree(target)
